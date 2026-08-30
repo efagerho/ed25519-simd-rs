b67a7d7b38a3d006ac743138bc80a25dd4cda338
Edvard Fagerholm <edvard.fagerholm@gmail.com>
2026-08-30 17:51:07 +0000

    Centralize comparison benchmark registration

--- benches-compare/benches/dalek_hot.rs
+++ benches-compare/benches/dalek_hot.rs
@@ -1,15 +1,5 @@
-use criterion::{Criterion, criterion_group, criterion_main};
 use ed25519_simd::DalekPolicy;
 
 pub mod support;
 
-fn ragged_batches(c: &mut Criterion) {
-    support::hot_ragged_batches::<DalekPolicy>(c);
-}
-
-fn hot_keys_4(c: &mut Criterion) {
-    support::hot_keys_4::<DalekPolicy>(c);
-}
-
-criterion_group!(benches, ragged_batches, hot_keys_4);
-criterion_main!(benches);
+support::benchmark_main!(hot, DalekPolicy);
